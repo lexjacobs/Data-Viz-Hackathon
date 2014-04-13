796849ec8386b4add35e5ad51aef5d8f6efc4d11
lexjacobs <lex.jacobs@gmail.com>
2014-04-12 17:20:45 -0700

    cleans up console logs and comments.

--- script/generateVis.js
+++ script/generateVis.js
@@ -3,8 +3,6 @@
 //max SFlong: -122.418954
 //max SFlat: 37.804770
 
-// var dataset = testData;
-
 var datasetSize = 0;
 
 var datasetLoader = function(selectedDay){
@@ -14,7 +12,6 @@ var datasetLoader = function(selectedDay){
       result.push(tripData[i]);
     }
   }
-  // console.log('dataset length: ', result.length);
   return result;
 };
 
@@ -27,8 +24,6 @@ var generateVis = function(selectedDay){
 
   var xScaler = 17000;
   var yScaler = 17000;  
-  // var xScaler = 27000;
-  // var yScaler = 17500;
 
   var latScaler = function(n){
     return (n - 37.771058) * xScaler;
@@ -46,54 +41,31 @@ var generateVis = function(selectedDay){
 
   circles = svg.selectAll('circle')
     .attr('cy', function(d){
-      // console.log('selectedDay: ', selectedDay);
       if(d[4] === selectedDay || selectedDay === 'all'){
         return (w-latScaler(d[0]))-10;
-        // return latScaler(d[0])+10;
       }
     })
     .attr('cx', function(d){
-      // console.log('latd1', longScaler(d[1]));
       if(d[4] === selectedDay || selectedDay === 'all'){
-        // console.log(d[4]);
         return (h-longScaler(d[1]))-10;
-        // return longScaler(d[1])+10;
       }
     })
     .attr('r', 1);
-    // .attr('fill', 'rgb(2,111,204)');
 
   circles
     .transition()
     .delay(300)
     .duration(3400)
     .attr('cy', function(d){
-      // console.log('latd2', latScaler(d[2]));
-      // console.log(d[4]);
       if(d[4] === selectedDay){
         return (w-latScaler(d[2]))-10;
         // return latScaler(d[2])+10;
       }
     })
     .attr('cx', function(d){
-      // console.log('latd3', longScaler(d[3]));
-      // console.log(d[4]);
       if(d[4] === selectedDay){
         return (h-longScaler(d[3]))-10;
-        // return longScaler(d[3])+10;
       }
     })
     .attr('r', 1);
-
-    // console.log("num circles", d3.selectAll('circle').length);
-
-  // TODO: figure out exit/remove 
-  // http://bost.ocks.org/mike/join/
-  // http://stackoverflow.com/questions/14574364/on-click-event-d3-js-only-works-first-time
-
-  /*  circles
-      .exit()
-      .remove();
-*/
-    // console.log('done in generateVis');
 };
--- script/svgAppender.js
+++ script/svgAppender.js
@@ -1,15 +1,4 @@
-
-  // generateVis(2);
-
   $(function(){
-
-//     $('body').append(
-// '<a href="http://hackreactor.com"> \
-// <div id="hrBanner"><img style="position: fixed; top: 0; right: 0; border: 0;" \
-// src="http://i.imgur.com/x86kKmF.png" \
-// alt="Built at Hack Reactor"></div> \
-// </a>');
-    // $('#hrBanner').css('z-index', 99);
 
     $('.navbar-default').css('background-color', '#0070cd');
     $('.navbar-brand').css('color', '#fff');
@@ -19,7 +8,6 @@
 
     var dayTripUpdater = function(){
       var currentTrips = d3.selectAll('circle')[0].length;
-      console.log('currentTrips',currentTrips);
       $('.tripCount').text(currentTrips);
       $('.day').text(dayOfMonth);
 
@@ -27,16 +15,11 @@
 
     $('.launch').on('click', function(){
         d3.selectAll('circle').remove();
-      // console.log("day val", $('.day').val());
       var gV = ($('.day').val());
       dayOfMonth = gV;
-      // console.log("gV", gV);
-      // cicles.remove();
       gV = parseInt(gV, 10);
       generateVis(gV);
       dayTripUpdater();
-      // console.log(currentTrips);
-
     });
 
     var autoAdvance = function(){
@@ -51,7 +34,6 @@
           generateVis(dayOfMonth);
           dayTripUpdater();
           dayOfMonth++;
-          console.log('dayOfMonth',dayOfMonth);
           if(dayOfMonth >= 32){
             dayOfMonth = 1;
           }
